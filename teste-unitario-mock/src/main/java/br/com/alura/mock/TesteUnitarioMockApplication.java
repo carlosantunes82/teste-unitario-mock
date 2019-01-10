1fd948e9c38f25124b04917fb2a30529be0e6d07
Carlos Antunes <CEAntunes.WA@rd.com.br>
2019-01-10 19:25:08 -0200

    teste mock

---
+++ teste-unitario-mock/src/main/java/br/com/alura/mock/TesteUnitarioMockApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteUnitarioMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteUnitarioMockApplication.class, args);
+	}
+
+}
+
---
+++ teste-unitario-mock/src/test/java/br/com/alura/mock/TesteUnitarioMockApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.alura.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteUnitarioMockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
