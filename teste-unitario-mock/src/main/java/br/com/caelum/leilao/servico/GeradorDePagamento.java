d831d239f8af2b7e1426391e1a615783b8e10e49
Carlos Antunes <CEAntunes.WA@rd.com.br>
2019-01-11 19:13:16 -0200

    avanco

--- teste-unitario-mock/src/main/java/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ teste-unitario-mock/src/main/java/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -7,21 +7,31 @@ import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
+import br.com.caelum.leilao.infra.relogio.Relogio;
+import br.com.caelum.leilao.infra.relogio.RelogioDoSistema;
 
 public class GeradorDePagamento {
 
     private final RepositorioDePagamentos pagamentos;
     private final RepositorioDeLeiloes leiloes;
     private final Avaliador avaliador;
+	private final Relogio relogio;
+
+    public GeradorDePagamento(RepositorioDeLeiloes leiloes, 
+            RepositorioDePagamentos pagamentos, 
+            Avaliador avaliador, Relogio relogio) {
+        this.leiloes = leiloes;
+        this.pagamentos = pagamentos;
+        this.avaliador = avaliador;
+		this.relogio = relogio;
+    }
 
     public GeradorDePagamento(RepositorioDeLeiloes leiloes, 
             RepositorioDePagamentos pagamentos, 
             Avaliador avaliador) {
-        this.leiloes = leiloes;
-        this.pagamentos = pagamentos;
-        this.avaliador = avaliador;
+    	this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
     }
-
+    
     public void gera() {
 
         List<Leilao> leiloesEncerrados = leiloes.encerrados();
